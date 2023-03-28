e48ab6c5ef5d8f6d70d4b3fae1b5d5013b8e2286
levinhkygoat <leky.developer@gmail.com>
2023-03-28 09:23:50 +0700

    fix bug

--- js/Page Detail - Breadcrumbs - Image Gallery.js
+++ js/Page Detail - Breadcrumbs - Image Gallery.js
@@ -23,34 +23,45 @@ const smallNextIcon = document.querySelector(
 );
 
 const smallImagesContainer = document.querySelector(
-    ".product--detail .thumbnail .small-images-container"
-  );
+  ".product--detail .thumbnail .small-images-container"
+);
 
 smallImages.forEach((img) => {
   img.addEventListener("click", () => {
+    const activeImage = document.querySelector(
+      ".product--detail > .thumbnail .small-images-container .small-images .img.active"
+    );
+
     mainThumbnail.style.display = "block";
     mainVideo.style.display = "none";
     mainThumbnail.src = img.src;
-    mainVideo.src = '';
+    mainVideo.src = "";
+    activeImage.classList.remove("active");
+    img.parentElement.classList.add("active");
   });
 });
 
 videoImageBtn.addEventListener("click", () => {
+  const activeImage = document.querySelector(
+    ".product--detail > .thumbnail .small-images-container .small-images .img.active"
+  );
   mainVideo.style.display = "block";
   mainVideo.src = videoImageBtn.parentElement.querySelector("iframe").src;
   mainThumbnail.style.display = "none";
-})
+  activeImage.classList.remove("active");
+  videoImageBtn.parentElement.classList.add("active");
+});
 
 smallNextIcon.addEventListener("click", () => {
-    const scrollWidth = document.querySelector(
-        ".product--detail .thumbnail .small-images .img"
-      ).offsetWidth;
-    smallImagesContainer.scrollLeft += scrollWidth;
-})
+  const scrollWidth = document.querySelector(
+    ".product--detail .thumbnail .small-images .img"
+  ).offsetWidth;
+  smallImagesContainer.scrollLeft += scrollWidth;
+});
 
 smallPrevIcon.addEventListener("click", () => {
-    const scrollWidth = document.querySelector(
-        ".product--detail .thumbnail .small-images .img"
-      ).offsetWidth;
-    smallImagesContainer.scrollLeft -= scrollWidth;
-})
+  const scrollWidth = document.querySelector(
+    ".product--detail .thumbnail .small-images .img"
+  ).offsetWidth;
+  smallImagesContainer.scrollLeft -= scrollWidth;
+});
